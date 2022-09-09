079828188c6429168148640b1042622a3e322bdb
Seth Graham <109529595+SethGrah@users.noreply.github.com>
2022-09-09 10:44:48 -0400

    commit

--- src/main/java/com/skillstorm/hook/controllers/GoalController.java
+++ src/main/java/com/skillstorm/hook/controllers/GoalController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,15 +23,13 @@ import com.skillstorm.hook.services.GoalService;
 
 @RestController
 @RequestMapping("/goals")
+@CrossOrigin
 public class GoalController {
 
 	private static final Logger log = LoggerFactory.getLogger(GoalController.class);
 	
 	@Autowired
 	private GoalService goalService;
-	
-	
-
 	
 	@GetMapping(path="/user/{user}")
 	public ResponseEntity<List<Goal>> findAllByUser(@PathVariable String user){
--- src/main/java/com/skillstorm/hook/controllers/UsersController.java
+++ src/main/java/com/skillstorm/hook/controllers/UsersController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.skillstorm.hook.services.UsersService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class UsersController {
 
 	private static final Logger log = LoggerFactory.getLogger(GoalController.class);
